8936dbdbee884cdfa6f76243127239fe396ed186
4338174 <4338174@rts-365.dk>
2017-09-13 16:12:57 +0200

    q

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 // Galleria start and load Theme
-//(function() { 
-//    Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
-//    Galleria.run('.galleria');
-//}());
+(function() {
+    Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
+    Galleria.run('.galleria');
+}());
 
+$(document).ready(function(){
+	$('#menubtn1').click(function(){
+		$(this).toggleClass('is-active');
+	});
+    $('#menubtn2').click(function(){
+		$(this).toggleClass('is-active');
+	});
+    $('#menubtn3').click(function(){
+		$(this).toggleClass('is-active');
+	});
+    $('#menubtn4').click(function(){
+		$(this).toggleClass('is-active');
+	});
+});
 
 //Panel snap (scroll snapping)
 jQuery(function($) {
@@ -17,7 +31,11 @@ jQuery(function($) {
 
 } ) ( jQuery );
 //controller.toggle( 'id-1' );
-$("#toggle").on( 'click', function () {controller.toggle('id-1')});
+$("#menubtn1").on( 'click', function () {controller.toggle('id-1')});
+$("#menubtn2").on( 'click', function () {controller.toggle('id-1')});
+$("#menubtn3").on( 'click', function () {controller.toggle('id-1')});
+$("#menubtn4").on( 'click', function () {controller.toggle('id-1')});
+//var page2 = () => {$('.container').panelSnap('snapToPanel', $('.panel_two'))}
 //Scroll snap Options
 //  jQuery(function($) {
 //    var options = {
@@ -47,3 +65,22 @@ $("#toggle").on( 'click', function () {controller.toggle('id-1')});
 //
 //    $('.panel_container').panelSnap(options);
 //  });
+
+$('.p2link').on('click', function() {
+  var $target = $('.panel_two');
+  $('.container').panelSnap('snapToPanel', $target);
+});
+
+$('.p3link').on('click', function() {
+  var $target = $('.panel_three');
+  $('.container').panelSnap('snapToPanel', $target);
+});
+
+$('.p4link').on('click', function() {
+  var $target = $('.panel_four');
+  $('.container').panelSnap('snapToPanel', $target);
+});
+//
+//$('#p2link').on('click', function() {
+//  $('.container').panelSnap('snapTo', 'next');
+//});
